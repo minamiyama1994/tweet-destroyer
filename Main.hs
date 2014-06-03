93da2c0a8c92a3a020f0abce37033358620c6210
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-06-04 03:54:48 +0900

    fix

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Control.Concurrent
 import qualified Data.ByteString.Char8 as DBC
 import Control.Monad
 import Web.Twitter.Conduit
@@ -6,6 +7,7 @@ import Web.Authenticate.OAuth
 import Control.Monad.Logger
 import qualified Web.Twitter.Types as TT
 import qualified Data.Text as T
+import Control.Monad.Error.Class
 
 tokens :: String -> String -> OAuth -- TwitterはアクセスするのにOAuthという仕組みを使っているのですが、その時に必要なConsumerKey/ConsumerSecretを指定します
 tokens ck cs = twitterOAuth
@@ -35,8 +37,12 @@ main = do
     putStr "何ふぁぼ以上のツイートを削除せずに残しますか？　nを入力すると、n以上のふぁぼられツイートは削除されません > " >> hFlush stdout
     cnt <- getLine >>= return . read
     ids <- getContents >>= return . map ( read . read ) . words
-    runNoLoggingT . runTW ( twInfo ck cs ot ots ) $ forM_ ids $ \ i -> do
-        status <- call $ showId i
-        when ( ( TT.statusFavoriteCount status < cnt ) || ( ( TT.userScreenName $ TT.statusUser status ) /= T.pack sn ) ) $ do
-            call $ destroyId $ i
-            return ( )
+    forM_ ids $ \i -> flip catchError ( \ _ -> putStrLn $ "ツイート" ++ show i ++ "の削除に失敗しました、処理を続行します" ) $ do
+        threadDelay $ 5 * 1000 * 1000
+        runNoLoggingT . runTW ( twInfo ck cs ot ots ) $ do
+            status <- call $ showId i
+            when ( ( TT.statusFavoriteCount status < cnt ) || ( ( TT.userScreenName $ TT.statusUser status ) /= T.pack sn ) ) $ do
+                call $ destroyId $ i
+                return ( )
+        putStrLn $ "ツイート" ++ show i ++ "を削除しました"
+    putStrLn $ "処理が終了しました"
